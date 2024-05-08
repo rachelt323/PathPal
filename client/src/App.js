7978129f7f138910285e4786966016505a9574c0
jasn6 <110363877+jasn6@users.noreply.github.com>
2024-05-08 12:44:41 -0400

    Register Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </Router>
   );
