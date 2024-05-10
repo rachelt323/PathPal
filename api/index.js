3d99dc88ae94ebfadf3a1e54ff224411f1d4078c
jasn6 <110363877+jasn6@users.noreply.github.com>
2024-05-09 23:14:52 -0400

    Plan Endpoints

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const checkAuth = require("./utils/checkAuth.js");
 const authRoutes = require("./routes/auth.js");
+const pathRoutes = require("./routes/plan.js");
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
+router.use("/plan", checkAuth, pathRoutes);
 
 module.exports = router;
---
+++ api/routes/plan.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const Plan = require("../models/Plan.js");
+
+const planRouter = express.Router();
+
+planRouter.post("/create", async (req, res) => {
+  try {
+    var newPlan = await Plan.create({
+      owner: req.user.id,
+      title: req.body.title,
+      place: req.body.place,
+      lat: req.body.lat,
+      lng: req.body.lng,
+    });
+
+    res.status(200).json(newPlan);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+planRouter.get("/:id", async (req, res) => {
+  try {
+    var userPlan = await Plan.findOne({ _id: req.params.id });
+    if (!userPlan) {
+      return res.status(404).send({ message: "Plan not found" });
+    }
+
+    res.status(200).json(userPlan);
+  } catch (error) {
+    res.status(500).send({ message: "Server error", error });
+  }
+});
+
+module.exports = planRouter;
