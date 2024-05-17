f92e59ec6da07f31728f534bceb68a6079258800
jasn6 <110363877+jasn6@users.noreply.github.com>
2024-05-17 12:20:47 -0400

    placesCaches routing

--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const checkAuth = require("./utils/checkAuth.js");
 const authRoutes = require("./routes/auth.js");
 const pathRoutes = require("./routes/plan.js");
+const placesCacheRoutes = require("./routes/placesCache.js");
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/plan", checkAuth, pathRoutes);
+router.use("/placesCache", checkAuth, placesCacheRoutes);
 
 module.exports = router;
