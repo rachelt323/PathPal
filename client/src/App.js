92f9fb52db3923b1d74ae71551b7bd6169f43a79
jasn6 <110363877+jasn6@users.noreply.github.com>
2024-05-21 13:35:37 -0400

    Authentication context util function
    keep the authentication status centrally managed and available to all components, reducing redundant network requests by handling the authentication check at the top level and only on application load or state change.

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,21 @@ import LoginPage from "./pages/LoginPage";
 import CreatePage from "./pages/CreatePage";
 import Plan from "./pages/Plan";
 import ExplorePage from "./pages/ExplorePage";
+import { AuthProvider } from "./util/AuthContext";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/plan/create" element={<CreatePage />} />
-        <Route path="plan/:planCode" element={<Plan />} />
-        <Route path="plan/:planCode/explore" element={<ExplorePage />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/plan/create" element={<CreatePage />} />
+          <Route path="plan/:planCode" element={<Plan />} />
+          <Route path="plan/:planCode/explore" element={<ExplorePage />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
