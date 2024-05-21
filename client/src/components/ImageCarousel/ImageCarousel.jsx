dbbc1844ae00cea8add58f31c643ef4183dd7f95
jasn6 <110363877+jasn6@users.noreply.github.com>
2024-05-21 13:25:29 -0400

    update to imageCarousel component
    css was messing up if the user had less than 3 plans

--- client/src/components/ImageCarousel/ImageCarousel.jsx
+++ client/src/components/ImageCarousel/ImageCarousel.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import PlanCard from "../PlanCard/PlanCard";
-import "./ImageCarousel.css";
 
 const useStyles = {
   carouselContainer: {
