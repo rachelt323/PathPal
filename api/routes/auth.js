1d57e427d43382221b978cb2621c61efeb8aeea7
jasn6 <110363877+jasn6@users.noreply.github.com>
2024-05-21 12:48:06 -0400

    logout and status authentication endpoints

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -5,6 +5,40 @@ const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
+router.get("/status", async (req, res) => {
+  try {
+    // Extract the JWT from the cookie
+    const token = req.cookies.access_token;
+    if (!token) {
+      // If no token is found, the user is not logged in
+      return res.status(401).json({ isLoggedIn: false });
+    }
+
+    // Verify the token
+    jwt.verify(token, process.env.JWT_CODE, (err, decoded) => {
+      if (err) {
+        // If token verification fails, the user is not logged in
+        return res.status(401).json({ isLoggedIn: false });
+      }
+
+      // If token is valid, return user data and logged in status
+      // `decoded` is the object that was previously encoded into the JWT
+      return res.json({
+        isLoggedIn: true,
+        user: {
+          id: decoded.id,
+          firstName: decoded.firstName,
+          lastName: decoded.lastName,
+          email: decoded.email,
+        },
+      });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ isLoggedIn: false, error: "Internal Server Error" });
+  }
+});
+
 router.post("/register", async (req, res) => {
   try {
     const newPassword = await bcryptjs.hash(req.body.password, 10);
@@ -51,6 +85,11 @@ router.post("/login", async (req, res) => {
   } else {
     return res.status(401).json({ error: "Invalid Password" });
   }
+});
+
+router.get("/logout", async (req, res) => {
+  res.clearCookie("access_token");
+  return res.status(200).json({ message: "logout success" });
 });
 
 module.exports = router;
