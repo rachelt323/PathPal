8aab5b6a853b2d75771489461f002eca3f51f55c
jasn6 <110363877+jasn6@users.noreply.github.com>
2024-05-24 23:19:47 -0400

    additional list endpoints (rearranging places in list)
    allows for manipulation of places inside list, mainly to allow user to rearrange the places inside a list.

--- api/routes/list.js
+++ api/routes/list.js
@@ -54,4 +54,66 @@ ListRouter.put("/:id", async (req, res) => {
   res.status(200).json({ message: "success" });
 });
 
+ListRouter.put("/:listId/addPlace", async (req, res) => {
+  try {
+    const { listId } = req.params;
+    const { place } = req.body;
+    const updatedList = await List.findByIdAndUpdate(listId, {
+      $push: { places: place },
+    });
+    res.json(updatedList);
+  } catch (error) {
+    console.error("Failed to add place:", error);
+    res.status(500).send(error);
+  }
+});
+
+ListRouter.put("/:listId/rearrange", async (req, res) => {
+  try {
+    const { listId } = req.params;
+    const { newOrder } = req.body;
+    const updatedList = await List.findByIdAndUpdate(
+      listId,
+      { $set: { places: newOrder } },
+      { new: true }
+    );
+    res.json(updatedList);
+  } catch (error) {
+    console.error("Failed to rearrange places:", error);
+    res.status(500).send(error);
+  }
+});
+
+ListRouter.delete("/:listId/:placeId/deletePlace", async (req, res) => {
+  try {
+    const { listId, placeId } = req.params;
+
+    const updatedList = await List.findByIdAndUpdate(
+      listId,
+      { $pull: { places: { location_id: placeId } } },
+      { new: true }
+    );
+    if (updatedList) {
+      res.json(updatedList);
+    } else {
+      res.status(404).send("List not found");
+    }
+  } catch (error) {
+    console.error("Failed to delete place:", error);
+    res.status(500).send(error);
+  }
+});
+
+ListRouter.get("/:id/places", async (req, res) => {
+  try {
+    var userList = await List.findOne({ _id: req.params.id });
+    if (!userList) {
+      return res.status(400).send({ message: "List not found" });
+    }
+    res.status(200).json(userList.places);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 module.exports = ListRouter;
