d799a431a1ca24a2355d8d2e9c1c6a56c8c71209
Rachel Tsui <rachelt3835@gmail.com>
2024-10-11 16:27:40 -0400

    Commited

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
-import CreatePage from "./pages/CreatePage";
 import Plan from "./pages/PlanPage/Plan.jsx";
 import ExplorePage from "./pages/ExplorePage/ExplorePage";
 import { AuthProvider } from "./util/AuthContext";
@@ -15,7 +14,6 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/plan/create" element={<CreatePage />} />
           <Route path="plan/:planCode" element={<Plan />} />
           <Route path="plan/:planCode/explore" element={<ExplorePage />} />
         </Routes>
